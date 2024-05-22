733841b45d2e668566eddc6d5ee1f8b18a53be3e
emiriko <notalvaroaustin@gmail.com>
2024-05-22 16:59:14 +0700

    feat: created repository

--- src/main/java/cs/ui/ac/id/advprog/eshop/model/Product.java
+++ src/main/java/cs/ui/ac/id/advprog/eshop/model/Product.java
@@ -1,4 +1,12 @@
 package cs.ui.ac.id.advprog.eshop.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
 }
